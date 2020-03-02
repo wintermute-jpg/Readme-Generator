f6d37275e7e2080f2f67909df04e9b64a598f10a
wintermute-jpg <58561062+wintermute-jpg@users.noreply.github.com>
2020-03-02 12:12:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,62 @@
+//required packages
 var inquirer = require("inquirer");
 var axios = require("axios");
 
 
+//Username Prompt
 inquirer.prompt([
     {
         type: "input",
         message: "What is your Github username?",
         name: "username",
     },
-]).then(function(response){
-    axios({
-        url: "https://api.github.com/search/users?q="+ response.username, 
-        method: "get",
-    }).then(function(response){
-        console.log(response);
-    });
+    //axios call using User name 
+]).then(response =>{
+    axios.get("https://api.github.com/search/users?q="+ response.username)
+    .then(response => {
+        const Data = (response.data.items);
+        //console.log(Data);
+        const avatar = Data.map(item => item.avatar_url);
+        console.log(avatar);
+    }).then(
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the title of your project?",
+                name: "title",
+            },
+            {
+                type: "input",
+                message: "How would you describe your project?",
+                name: "description",
+            },
+            //Table of Contents needs specific formatting
+            {
+    
+            },
+            //Installation Instructions 
+            {
+                type: "input",
+                message: "How do you install your project?",
+                name: "install",
+            },
+            //Usage documentation
+            {
+                type: "input",
+                message: "Please ID the Usage for your project",
+                name: "usage",
+            },
+            //Lisence
+            {
+
+            },
+        ])
+    );
 });
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is your projects title?",
-        name: "title",
-    },
-    {
-       type: "input",
-       message: "Can you describe your project?",
-       name: "Description", 
-    },
-    {
-        type: "input",
-        message: "What kind of license are you using?",
-        name: "License",
-    },
-    {
-        type: "input",
-        message: "Who contributed to this project?",
-        name: "Contributing",
-    },
-])
+
 
 
 //To Do:
-//figure out gitignore so that I can actually update the repo.
-//figure out how to upload to github repo without computer begging for death
-//correct github API notation so that the call works
 //fill in the rest of the prompts 
 //create patchwork ReadMe format that can be filled in with prompt answers.
